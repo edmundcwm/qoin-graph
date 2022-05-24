51ee9fe867c01847dba82f9b9632bcc6d17d26d1
Edmund Chan <edmundcwm@gmail.com>
2022-05-24 10:32:19 +0800

    Fix linting

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,23 +1,27 @@
-(function() {
+( function() {
+	const addCurrencyBtn = document.getElementById( 'add-new-currency' );
+	const currencyTable = document.getElementById( 'currency-table' );
+	const rowClone = document.getElementById( 'clone-row' );
+	const tableBody = document.getElementById( 'the-list' );
+	const currencyCodeEl = document.getElementById( 'currency-code' );
+	const currencySymbolEl = document.getElementById( 'currency-symbol' );
 
-    const addCurrencyBtn = document.getElementById('add-new-currency');
-	const currencyTable = document.getElementById('currency-table');
-	const rowClone = document.getElementById('clone-row');
-	const tableBody = document.getElementById('the-list');
-	const currencyCodeEl = document.getElementById('currency-code');
-	const currencySymbolEl = document.getElementById('currency-symbol');
-	
-    if ( ! addCurrencyBtn || ! currencyTable ) {
-        return;
-    }
+	if ( ! addCurrencyBtn || ! currencyTable || rowClone || tableBody || currencyCodeEl || currencySymbolEl ) {
+		return;
+	}
 
+	/**
+	 * Create a new row in the currency table.
+	 *
+	 * @return {HTMLTableRowElement} cloned table row.
+	 */
 	function createNewCurrencyRow() {
-		const newRow = rowClone.cloneNode(true)
-		newRow.removeAttribute('id');
-		newRow.classList.remove('hidden');
+		const newRow = rowClone.cloneNode( true );
+		newRow.removeAttribute( 'id' );
+		newRow.classList.remove( 'hidden' );
 
-		const currencyCode = newRow.querySelector('.currency-row__code');
-		const currencySymbol = newRow.querySelector('.currency-row__symbol2');
+		const currencyCode = newRow.querySelector( '.currency-row__code' );
+		const currencySymbol = newRow.querySelector( '.currency-row__symbol' );
 
 		if ( ! currencyCode || ! currencySymbol ) {
 			return false;
@@ -29,8 +33,11 @@
 		return newRow;
 	}
 
-    async function handleAddCurrency() {
-        const endpoint = '/wp-json/qoin-graph/v1/currencies';
+	/**
+	 * Handle adding of new currency.
+	 */
+	async function handleAddCurrency() {
+		const endpoint = '/wp-json/qoin-graph/v1/currencies';
 		const url = qoinGraphSettings.root + endpoint; //eslint-disable-line no-undef
 
 		try {
@@ -45,41 +52,41 @@
 					currencySymbol: currencySymbolEl.value,
 				} ),
 			} );
-	
-			const result = await response.json();
-	
+
+			await response.json();
+
 			// Add new row to the Currencies table after a successful response
 			if ( response.ok ) {
 				// Display table if it's currently hidden.
-				currencyTable.classList.remove('hidden');
-				
+				currencyTable.classList.remove( 'hidden' );
+
 				const newRow = createNewCurrencyRow();
-	
+
 				if ( ! newRow ) {
-					throw new Error('Could not create new row.');
+					throw new Error( 'Could not create new row.' );
 				}
 
-				tableBody.appendChild(newRow);
-				
+				tableBody.appendChild( newRow );
+
 				// Clear input fields
 				currencyCodeEl.value = '';
 				currencySymbolEl.value = '';
-	
-				return result;
 			}
-		} catch (err) {
-			console.error(err.message);
-		// 	const noticeWrapper = document.getElementById( 'new-currency-notice');
-		// 	noticeWrapper.classList.remove('hidden');
-
-		// 	const noticeMessageEl = document.querySelector( '#new-currency-notice .notice-message' );
-		// 	noticeMessageEl.textContent = err.message;
+		} catch ( err ) {
+			console.error( err.message );
 		}
-	
-    }
+	}
 
-    addCurrencyBtn.addEventListener( 'click', handleAddCurrency );
+	async function handleDeleteCurrency( event ) {
+		event.preventDefault();
+		if ( 'BUTTON' !== event.target.tagName || ! event.target.classList.contains( 'delete' ) ) {
+
+		}
+	}
+
+	addCurrencyBtn.addEventListener( 'click', handleAddCurrency );
+
+	currencyTable.addEventListener( 'click', handleDeleteCurrency );
 
 	//TODO Delete currency
-
-})();
+}() );
