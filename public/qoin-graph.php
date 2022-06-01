4743ce94a9765b746adcaeb5af848d1dd527ecf4
Edmund Chan <edmundcwm@gmail.com>
2022-06-01 15:23:39 +0800

    Add markup for Chart widget

--- public/qoin-graph.php
+++ public/qoin-graph.php
@@ -18,33 +18,29 @@ function shortcode_callback() {
 
 	?>
 
-	<div class="qcpw-container">
-		<div class="qcpw-list-header">
-			<?php
-			if ( ! empty( $currencies ) ) {
-				?>
-				<select id="currency-dropdown">
-					<?php foreach ( $currencies as $code => $symbol ) { ?>
-						<option value="<?php echo esc_attr( $code ); ?>" <?php selected( $code, 'AUD' ); ?>><?php echo esc_html( $code ); ?></option>
-					<?php } ?>
-				</select>
+	<div class="qg-wrapper">
+		<div class="qg-header">
+			<div class="qg-controls">
+				<button class="qg-controls__control" data-frequency="1M"><?php echo esc_html__( '1M', 'qoin-graph' ); ?></button>
+				<button class="qg-controls__control" data-frequency="3M"><?php echo esc_html__( '3M', 'qoin-graph' ); ?></button>
+				<button class="qg-controls__control" data-frequency="6M"><?php echo esc_html__( '6M', 'qoin-graph' ); ?></button>
+				<button class="qg-controls__control" data-frequency="all"><?php echo esc_html__( 'ALL', 'qoin-graph' ); ?></button>
 				<?php
-			}
-			?>
+				if ( ! empty( $currencies ) ) {
+					?>
+					<select id="currency-dropdown" class="qg-controls__control qg-controls__dropdown">
+						<?php foreach ( $currencies as $code => $symbol ) { ?>
+							<option value="<?php echo esc_attr( $code ); ?>" <?php selected( $code, 'AUD' ); ?>><?php echo esc_html( $code ); ?></option>
+						<?php } ?>
+					</select>
+					<?php
+				}
+				?>
+			</div>
 		</div>
-		<div class="qcpw-list-body">
-			<div class="qcpw-list-body__info">
-				<div class="qcpw-change-wrapper">
-					<span id="percentage-change" class="qcpw-percentage"></span>
-				</div>
-			</div>
-			<div class="qcpw-list-toggle">
-				<button class="qcpw-toggle-btn" data-frequency="hour"><?php echo esc_html__( '1D', 'qoin-graph' ); ?></button>
-				<button class="qcpw-toggle-btn" data-frequency="day"><?php echo esc_html__( '7D', 'qoin-graph' ); ?></button>
-				<button class="qcpw-toggle-btn" data-frequency="month"><?php echo esc_html__( '1M', 'qoin-graph' ); ?></button>
-			</div>
-			<img class="hidden" id="loader" src="<?php echo esc_url( get_admin_url() . 'images/loading.gif' ); ?>" alt="loading" />
-			<canvas id="qcpw-chart" width="340" height="100"></canvas>
+		<div class="qg-body">
+			<img class="" id="qg-loader" src="<?php echo esc_url( get_admin_url() . 'images/loading.gif' ); ?>" alt="loading" />
+			<canvas id="qg-chart" width="340" height="100"></canvas>
 		</div>
 	</div>
 
